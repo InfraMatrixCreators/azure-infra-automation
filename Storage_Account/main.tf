ae2dc7dfc9265a8755fea3e6f709426885415e6c
Jitendar88 <jitendarsingh04@gmail.com>
2025-09-29 11:09:12 +0530

    st account

---
+++ Storage_Account/main.tf
@@ -0,0 +1,11 @@
+resource "azurerm_resource_group" "rg_name" {
+  name     = "rg-terraform-demo"
+  location = "Australia East"
+}
+resource "azurerm_storage_account" "st_name" {
+  name                     = "sttestlogswus01"
+  resource_group_name      = azurerm_resource_group.rg_name.name
+  location                 = azurerm_resource_group.rg_name.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
