e6a5e92a2f9e0ce1544f469311654d6d19a79e5a
Jitendar88 <jitendarsingh04@gmail.com>
2025-11-05 08:19:50 +0530

    rg_india added

---
+++ rg_india/main.tf
@@ -0,0 +1,4 @@
+resource "azurerm_resource_group" "rg_india" {
+  name     = "rg-india-001"
+  location = "India South"
+}
---
+++ rg_india/provider.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "4.51.0"
+    }
+  }
+}
+
+provider "azurerm" {
+    features {}
+    subscription_id = cba3453a-ff1f-4f51-b6f6-5a1149843776
+}
